2a3cabe204be713808f8d76639aec9b5a367dd9c
william <myelvesgrug@gmail.com>
2022-06-01 08:45:36 +0700

    adding typescript

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,37 @@
 import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import Web3Modal from "web3modal";
 import styles from '~/styles/Layout/Header.module.css'
 
-// const providerOptions = {
-//   /* See Provider Options Section */
-// };
 
-// const web3Modal = new Web3Modal({
-//   network: "mainnet", // optional
-//   cacheProvider: true, // optional
-//   providerOptions // required
-// });
 
-// const instance = await web3Modal.connect();
+const providerOptions = {
+    /* See Provider Options Section */
+}; 
 
-// const provider = new ethers.providers.Web3Provider(instance);
-// const signer = provider.getSigner();
 
 function Header() {
+    const [web3Modal, setWeb3Modal] = useState({})
+    
+    useEffect(() => {
+        setWeb3Modal(new Web3Modal({
+            network: "rinkeby", // optional
+            providerOptions // required
+        }))
+        // const web3Modal = 
+    }, []);
+
+    
+    async function connectWallet() {
+        const instance = await web3Modal.connect();
+
+        const provider = new ethers.providers.Web3Provider(instance);
+        const signer = provider.getSigner();
+        const account = await provider.listAccounts();
+        const balance = await provider.getBalance(account[0]);
+        console.log(balance.toString())
+    }
+
     return (
         <div className={styles.header}>
             <div className="grid grid-cols-4 gap-4">
@@ -25,7 +39,7 @@ function Header() {
                 <div>09</div>
             </div>
             <div>
-                <button>connect</button>
+                <button onClick={connectWallet}>connect</button>
             </div>
             
         </div>
