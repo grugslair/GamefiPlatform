d1878093ce3a1959f6d6f8d905d4357985d7289e
william <myelvesgrug@gmail.com>
2022-06-01 11:21:13 +0700

    change project to typescript

--- store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import walletReducer from './walletReducer'
-
-export const store = configureStore({
-  reducer: {
-      wallet: walletReducer
-  },
-})
