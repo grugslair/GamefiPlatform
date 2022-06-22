81593055f9b7303cfb1b8441dbe7d861bcbdc38c
william <myelvesgrug@gmail.com>
2022-06-22 22:22:27 +0700

    revert back using flowbite

---
+++ components/Home/Project/ProjectBanner/index.tsx
@@ -0,0 +1,18 @@
+const ProjectBanner = () => {
+  return (
+    <>
+      <div className="relative">
+        <img></img>
+
+        <div className="absolute">
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProjectBanner
---
+++ components/Home/Project/index.tsx
@@ -0,0 +1,13 @@
+import ProjectBanner from "./ProjectBanner"
+
+const Project = () => {
+  return (
+    <>
+      <div className="px-2">
+        <ProjectBanner></ProjectBanner>
+      </div>
+    </>
+  )
+}
+
+export default Project
