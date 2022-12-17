808a15fdd24b676b1d3b9935d5e1acb9cd94a657
william karno <myelvesgrug@gmail.com>
2022-12-17 23:21:42 +0700

    change text

--- components/Public/ModalClaimRocksButton/index.tsx
+++ components/Public/ModalClaimRocksButton/index.tsx
@@ -131,7 +131,7 @@ const ModalClaimRocksButton = ({
               {formatNumber(contractClaim.unClaimNft.length)} / {formatNumber(wallet.balance || 0)} &nbsp; 
               Grug(s) available to claim
             </span>
-            .&nbsp;Each Grug contain 3,000 $ROCKS
+            .&nbsp;You can claim 3,000 $ROCKS per Grug ownerd
           </div>
           <div className="mt-4 border border-solid border-[#CA5D504D] bg-grugAltCardBackground10 p-4 pt-6">
             <div className="text-sora text-xs font-light text-gray300">
