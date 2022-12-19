cbae3d2c7b692393ed234307243a6ed904c169e2
Willy Alfianto <frosthaki@gmail.com>
2022-12-19 23:04:56 +0700

    remove dynamic imports

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,10 +1,5 @@
-import dynamic from 'next/dynamic'
 import Footer from './Footer'
-// import Header from './Header'
-
-const Header = dynamic(() => import('./Header'), {
-  ssr: false
-})
+import Header from './Header'
 
 export default function Layout({ children }: any) {
   return (
