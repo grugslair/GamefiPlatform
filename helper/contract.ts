667c6568bb23c51ff8c8a27bb56830436ab588e6
william karno <myelvesgrug@gmail.com>
2022-12-19 22:04:18 +0700

    update contract rocks for production

--- helper/contract.ts
+++ helper/contract.ts
@@ -10,7 +10,7 @@ const grugContractAddressENV: IContractEnv = {
 
 const rocksContractAddressENV: IContractEnv = {
   development: "0xB36Dd853f10EdEc6A029A06AC3641cCe78537c0D",
-  production: ''
+  production: '0xe6222fe5294d3546a0c32b028c5af9a2a60481da'
 } 
 
 const stakeContractAddressENV: IContractEnv = {
