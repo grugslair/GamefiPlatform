f654c03d29d05d9740fa679909abbce6549c3039
william karno <myelvesgrug@gmail.com>
2023-03-17 22:29:12 +0700

    update nftclaim error

--- store/contractClaim/index.ts
+++ store/contractClaim/index.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { IContractClaim } from './contractClaim'
-import { claimNFT, initiateContractClaim, isNFTClaimed } from './thunk'
+import { isNFTClaimed } from './thunk'
 
 const initialState = {
   contract: null,
@@ -20,15 +20,6 @@ const contractClaim = createSlice({
     builder.addCase(isNFTClaimed.fulfilled, (state: IContractClaim, action: any) => {
       state.claimedNft = action.payload.claimedNft
       state.unClaimNft = action.payload.unClaimNft
-    })
-
-    builder.addCase(claimNFT.fulfilled, (state: IContractClaim, action: any) => {
-      if(action.payload.receipt.transactionHash) {
-        for(let token = 0; token < action.payload.amount; token++) {
-          state.claimedNft.push(state.unClaimNft[token])
-          state.unClaimNft.shift()
-        }
-      }
     })
   }
 })
