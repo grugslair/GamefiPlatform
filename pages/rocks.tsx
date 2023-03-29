e4f73216e8223b8367040e267af482b8bb14bd92
Willy Alfianto <frosthaki@gmail.com>
2023-03-29 21:05:54 +0700

    push

--- pages/rocks.tsx
+++ pages/rocks.tsx
@@ -12,7 +12,7 @@ import { addRocksTokenToWallet, getRocksFromNFT } from "store/wallet/thunk";
 import { isNFTClaimed } from "store/contractClaim/thunk";
 import { IContractRocks } from "store/contractRocks/contractRocks";
 import { IContractStake } from "store/contractStake/contractStake";
-import { contractUnstaking, getGasPrice } from "store/contractStake/thunk";
+import { contractUnstaking, getAvailableWithdrawAmount, getGasPrice } from "store/contractStake/thunk";
 
 // Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -41,6 +41,8 @@ const Staking: NextPage = () => {
 
   const [unStakeAmount, setUnStakeAmount] = useState("");
 
+  const [loadingUnstake, setLoadingUnstake] = useState<boolean>(false);
+
   const unstakeDisabled = !(contractRocks?.balanceOfRocks > 0);
 
   function changeUnStakeAmount(value: string) {
@@ -56,15 +58,16 @@ const Staking: NextPage = () => {
 
   async function unStake() {
     await dispatch(getGasPrice());
+    setLoadingUnstake(true)
     const weiAmount = ethToWei(unStakeAmount?.toString() || "0");
     const result = await dispatch(contractUnstaking(weiAmount));
 
-    if (result?.payload?.hash) {
+    if (result?.payload?.ts?.hash) {
       pushMessage(
         {
           status: "success",
           title: "",
-          description: "Successfully unstake token",
+          description: `Successfully unstake ${unStakeAmount} token`,
         },
         dispatch
       );
@@ -81,6 +84,9 @@ const Staking: NextPage = () => {
         dispatch
       );
     }
+
+    await dispatch(getAvailableWithdrawAmount());
+    setLoadingUnstake(false)
   }
 
   useEffect(() => {
@@ -98,7 +104,12 @@ const Staking: NextPage = () => {
           size="small"
           onClick={onClickAddRocksToken}
         >
-          <Image src={"/metamask.svg"} alt="metamask" width={18} height={18} />
+          <Image
+            src={"/icons/metamask.svg"}
+            alt="metamask"
+            width={18}
+            height={18}
+          />
           Add $ROCKS
         </Button>
       </div>
@@ -181,6 +192,7 @@ const Staking: NextPage = () => {
                 <Button
                   size="small"
                   onClick={unStake}
+                  loading={loadingUnstake}
                   disabled={unstakeDisabled}
                 >
                   Unstake
